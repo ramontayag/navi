d5c7670376876b91c3d6fc73553a7594ddb4df2b
Ramon Tayag <ramon@tayag.net>
2012-02-19 11:30:48 +0800

    Fixes being able to delete navigable items

--- spec/navi/navigable_spec.rb
+++ spec/navi/navigable_spec.rb
@@ -2,22 +2,34 @@ require 'spec_helper'
 
 describe "Navigable" do
   context "when deleted" do
-    before do
-      page = Factory :page, :name => "Page name"
-      @menu_item = page.to_navigator!
-      page.destroy
-      @menu_item.reload
+    context "and the navigator has a navigable item" do
+      before do
+        page = Factory :page, :name => "Page name"
+        @menu_item = page.to_navigator!
+        page.destroy
+        @menu_item.reload
+      end
+
+      it "should nullify the navigator's (menu item) pointer to itself" do
+        @menu_item.navigable.should be_nil
+        @menu_item.navigable_id.should be_nil
+        @menu_item.navigable_type.should be_nil
+      end
+
+      it "should set the label and url" do
+        @menu_item.label.should == "Page name"
+        @menu_item.url.should == "#"
+      end
     end
 
-    it "should nullify the navigator's (menu item) pointer to itself" do
-      @menu_item.navigable.should be_nil
-      @menu_item.navigable_id.should be_nil
-      @menu_item.navigable_type.should be_nil
-    end
+    context "and the navigable has no navigator item" do
+      before do
+        @page = Factory :page, :name => "Page name"
+      end
 
-    it "should set the label and url" do
-      @menu_item.label.should == "Page name"
-      @menu_item.url.should == "#"
+      it "should not blow up" do
+        expect {@page.destroy}.to_not raise_error
+      end
     end
   end
 end
